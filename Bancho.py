3dea2e9e981b76813e2acbb934104900adde2a82
C-L-T <32127694+C-L-T@users.noreply.github.com>
2017-09-20 16:16:25 +0100

    Add files via upload

---
+++ Bancho.py
@@ -0,0 +1,15 @@
+#CatSu! Bancho
+#Development build 0.01
+#GitHub: https://github.com/C-L-T/CatSu
+
+import os
+import sys
+import time
+
+f = open("Launch.txt", "r")
+intro = f.read()
+print (intro)
+
+time.sleep(1)
+
+print("\nInitialising Bancho...")
